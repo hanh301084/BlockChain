b3c91b85c086517bd9316fc095b4126a97136e03
hanh301084 <nha302031@gmail.com>
2021-05-17 16:14:27 +0700

    hash Private Key for storage

--- controllers/index.js
+++ controllers/index.js
@@ -1,17 +1,18 @@
 const EC = require('elliptic').ec; // tao khóa, ký và xác minh chữ ký 
 const ec = new EC('secp256k1'); // thuật toán của EC
+const SHA256 = require('crypto-js/sha256');
 const keyModel =require('../model/key');
 
 exports.createWallet = async (req, res) => {
     const keyPair = ec.genKeyPair();
     const key = new keyModel({
         PublicKey: keyPair.getPublic('hex'),
-        PrivateKey: keyPair.getPrivate('hex')
+        hashPrivateKey: SHA256(keyPair.getPrivate('hex')).toString()
     });
-    console.log(key);
+    console.log('1' + key);
     try{
-        const saveKey = await key.save();
-        res.render('signup', {key: key.PrivateKey});
+        const saveKey = await key.save(); console.log('2' + saveKey);
+        res.render('signup', {key: keyPair.getPrivate('hex')});
     }catch(err){
         res.json({ message: err });
     }
--- model/key.js
+++ model/key.js
@@ -1,16 +1,8 @@
 const mongoose = require('mongoose');
-const SHA256 = require('crypto-js/sha256');
 
 const KeySchema = mongoose.Schema({
-    PrivateKey: {type: String, require: true},
-    PublicKey: { type: String, require: true , unique: true}    
+    PublicKey: { type: String, require: true },
+    hashPrivateKey: {type: String, require: true}
 });
-
-KeySchema.methods.hashKey=function(PrivateKey){
-    return SHA256(PrivateKey).toString();
-}
-KeySchema.methods.validKey=function(PrivateKey){
-    return this.PrivateKey===SHA256(PrivateKey).toString();
-}
 
 module.exports=mongoose.model('Key',KeySchema, "Key");
